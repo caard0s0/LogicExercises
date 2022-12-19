a765638ea2b131a21401f1010f78fa1ad8d3b8ee
Vinicius <vinicardoso216@gmail.com>
2022-12-18 21:54:14 -0300

    feat(footballBetApp.js): Coding Challenge #12

--- codingChallenges/footballBetApp.js
+++ codingChallenges/footballBetApp.js
@@ -38,44 +38,44 @@ Then, call the function again with players from game.scored.
 */
 
 const game = {
-    team1: 'Bayern Munich',
-    team2: 'Borrussia Dortmund',
-    players: [
-      [
-        'Neuer',
-        'Pavard',
-        'Martinez',
-        'Alaba',
-        'Davies',
-        'Kimmich',
-        'Goretzka',
-        'Coman',
-        'Muller',
-        'Gnarby',
-        'Lewandowski',
-      ],
-      [
-        'Burki',
-        'Schulz',
-        'Hummels',
-        'Akanji',
-        'Hakimi',
-        'Weigl',
-        'Witsel',
-        'Hazard',
-        'Brandt',
-        'Sancho',
-        'Gotze',
-      ],
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
     ],
-    score: '4:0',
-    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-    date: 'Nov 9th, 2037',
-    odds: {
-      team1: 1.33,
-      x: 3.25,
-      team2: 6.5,
-    },
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
 };
 
 // 1.
@@ -95,11 +95,11 @@ const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
 console.log(players1Final);
 
 // 5.
-const {odds:{team1, x: draw, team2}} = game;
+const { odds: { team1, x: draw, team2 } } = game;
 console.log(team1, draw, team2);
 
 // 6.
-const printGoals = function(...players) {
+const printGoals = function (...players) {
   console.log(`${players.length} goals were scored.`);
 }
 printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
@@ -216,3 +216,61 @@ for (const player of game.scored) {
   scorers[player] ? scorers[player]++ : (scorers[player] = 1)
 }
 console.log(scorers);
+
+console.log('///////////////////////////////////////////////////////////');
+
+// Coding CHallenge #12
+
+/* 
+Let's continue with our football betting app! This time, we have a map called
+'gameEvents' (see below) with a log of the events that happened during the game. 
+The values are the events themselves, and the keys are the minutes in which
+each event happened (a football game has 90 minutes plus some extra time).
+
+
+YOUR TASKS:
+1. Create an array 'events' of the different game events that happened (no
+duplicates).
+
+2. After the game has finished, is was found that the yellow card from minute 64
+was unfair. So remove this event from the game events log.
+
+3. Compute and log the following string to the console: "An event happened, on
+average, every 9 minutes" (keep in mind that a game has 90 minutes).
+
+4. Loop over 'gameEvents' and log each element to the console, marking
+whether it's in the first half or second half (after 45 min) of the game, like this: [FIRST HALF] 17: ⚽ GOAL.
+*/
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// 1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3.
+const time = [...gameEvents.keys()].pop();
+const averageEvents = time / gameEvents.size;
+console.log(`An event happened, on average, every ${averageEvents} minutes`);
+
+// 4.
+for (const [min, event] of gameEvents.entries()) {
+  const half = min <= 45 ? '[FIRST HALF]' : '[SECOND HALF]';
+  console.log(`${half} ${min}: ${event}.`);
+}
