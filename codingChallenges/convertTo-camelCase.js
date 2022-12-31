62d2373b4138cda08d2b0d03cb26a8ce3ee4aa27
Vinicius <vinicardoso216@gmail.com>
2022-12-31 13:11:25 -0300

    refactor(convertTo-camelCase): organizing the code

--- codingChallenges/convertTo-camelCase.js
+++ codingChallenges/convertTo-camelCase.js
@@ -26,18 +26,22 @@ calculateAge       ✅✅✅✅
 delayedDeparture   ✅✅✅✅✅
 */
 
+console.log('---------------------------------------------');
+console.log('--------------- CHALLENGE #13 ---------------');
+console.log('---------------------------------------------');
+
 // 1.
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 
 document.querySelector('button').addEventListener('click', function () {
-    const text = document.querySelector('textarea').value;
-    const rows = text.split('\n');
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
 
-    for (const [i, row] of rows.entries()) {
-        const [first, second] = row.toLowerCase().trim().split('_');
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
 
-        const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
-        console.log(`${output.padEnd(20)} ${'✅'.repeat(i + 1)}`);
-    }
+    const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
+    console.log(`camelCase: ${output.padEnd(20)} ${'✅'.repeat(i + 1)}`);
+  }
 })
