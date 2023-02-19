75b677db2a94fa1aa9eeb0675e34bf6dc15ba20e
Vinicius <vinicardoso216@gmail.com>
2023-02-19 14:20:14 -0300

    feat(car.js): Coding Challenge #23

--- codingChallenges/car.js
+++ codingChallenges/car.js
@@ -202,3 +202,81 @@ tesla.brake();
 tesla.accelerate();
 
 console.log(tesla);
+
+
+
+// Coding Challenge #23
+
+/*
+YOUR TASKS: 
+1. Re-create Challenge #3, but this time using ES6 classes: create an 'EVCl'
+child class of the 'CarCl' class.
+
+2. Make the 'charge' property private.
+
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery'
+methods of this class, and also update the 'brake' method in the 'CarCl'
+class. Then experiment with chaining!.
+
+
+TEST DATA:
+Data car 1: 'Rivian' going at 120 km/h, with a charge of 23%
+*/
+
+console.log('---------------------------------------------');
+console.log('--------------- CHALLENGE #23 ---------------');
+console.log('---------------------------------------------');
+
+class NewCarCl {
+    constructor(make, speed) {
+        this.make = make;
+        this.speed = speed;
+    }
+
+    accelerate() {
+        this.speed += 10;
+        console.log(`${this.make} is going at ${this.speed} km/h`);
+    }
+
+    brake() {
+        this.speed -= 5;
+        console.log(`${this.make} is going at ${this.speed} km/h`);
+        return this;
+    }
+
+    get speedUS() {
+        return this.speed / 1.6;
+    }
+
+    set speedUS(speed) {
+        this.speed = speed * 1.6;
+    }
+};
+
+// 1.
+class EVCl extends NewCarCl {
+    // 2.
+    #charge;
+
+    constructor(make, speed, charge) {
+        super(make, speed)
+        this.#charge = charge;
+    };
+
+    chargeBattery(chargeTo) {
+        this.#charge = chargeTo;
+        return this;
+    };
+
+    accelerate() {
+        this.speed += 20;
+        this.#charge--;
+        console.log(`${this.make} going at ${this.speed} km/h, with a charge of ${this.#charge}%.`);
+        return this;
+    }
+};
+
+// 3.
+const rivian = new EVCl('Rivian', 120, 23);
+rivian.chargeBattery(90).brake().brake().brake().accelerate().accelerate();
+console.log(rivian);
