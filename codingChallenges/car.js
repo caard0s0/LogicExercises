1ec39a0766b079046aa3a30f7c04278d34c77e2c
Vinicius <vinicardoso216@gmail.com>
2023-02-19 14:11:55 -0300

    feat(car.js): Coding Challenge #22

--- codingChallenges/car.js
+++ codingChallenges/car.js
@@ -126,3 +126,79 @@ ford.brake();
 
 ford.speedUS = 100;
 console.log(ford);
+
+
+
+// Coding Challenge #22
+
+/*
+YOUR TASKS:
+1. Use a constructor function to implement an Electric Car (called 'EV') as a child
+"class" of 'Car'. Besides a make and current speed, the 'EV' also has the
+current battery charge in % ('charge' property).
+
+2. Implement a 'chargeBattery' method which takes an argument
+'chargeTo' and sets the battery charge to 'chargeTo'.
+
+3. Implement an 'accelerate' method that will increase the car's speed by 20,
+and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140
+km/h, with a charge of 22%'.
+
+4. Create an electric car object and experiment with calling 'accelerate',
+'brake' and 'chargeBattery' (charge to 90%).
+
+
+TEST DATA:
+Data car 1: 'Tesla' going at 120 km/h, with a charge of 23%.
+*/
+
+console.log('---------------------------------------------');
+console.log('--------------- CHALLENGE #22 ---------------');
+console.log('---------------------------------------------');
+
+/*
+const Car = function (make, speed) {
+    this.make = make;
+    this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+*/
+
+// 1.
+const EV = function (make, speed, charge) {
+    Car.call(this, make, speed);
+    this.charge = charge;
+};
+
+EV.prototype.constructor = EV;
+EV.prototype = Object.create(Car.prototype);
+
+// 2.
+EV.prototype.chargeBattery = function (chargeTo) {
+    this.charge = chargeTo;
+};
+
+// 3.
+EV.prototype.accelerate = function () {
+    this.speed += 20;
+    this.charge--;
+    console.log(`${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%.`);
+}
+
+// 4.
+const tesla = new EV('Tesla', 120, 23);
+tesla.chargeBattery(90);
+tesla.accelerate();
+tesla.brake();
+tesla.accelerate();
+
+console.log(tesla);
